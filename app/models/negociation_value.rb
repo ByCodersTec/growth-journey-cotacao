f67e304d36381d5a5445e93829dd01f193f4bb98
rodrigocezzar <rodrigocezzar85@gmail.com>
2021-08-26 22:46:47 -0300

    project creation, creation of models, controller and view.

---
+++ app/models/negociation_value.rb
@@ -0,0 +1,3 @@
+class NegociationValue < ApplicationRecord
+  
+end
---
+++ app/models/quotation.rb
@@ -0,0 +1,41 @@
+class Quotation < ApplicationRecord
+  belongs_to :user, optional: true
+
+  # validate_presence_of :value, :installment, :form_payment
+
+  # Callbacks
+  before_create :calculate_factory_cost
+  before_save :calculate_value_with_discount
+  before_save :calculate_commissions
+  before_save :calculate_other_values
+  before_save :calculate_taxs  
+
+  def calculate_factory_cost
+    self.factory_cost = value * (Setup.first.factory_cost)/100
+  end
+
+  # Calcula valor desconto
+  def calculate_value_with_discount
+    self.value = value - (value * (discount/100))
+  end
+
+  # commissions
+  def calculate_commissions
+    self.assembler_commission = value * (Setup.first.assembler_commission)/100
+    self.manager_commission = value * (Setup.first.manager_commission)/100
+    #self.seller_commission = value * (Setup.first.seller_commission)/100
+  end
+
+  # other_values = transporte, reserva técnica
+  def calculate_other_values
+    self.shipping_cost = factory_cost * (Setup.first.shipping_cost)/100
+    self.reserve_cost = value * (Setup.first.reserve_cost)/100
+  end
+
+  # tax
+  def calculate_taxs
+      self.tax = value * (Setup.first.tax)/100
+      #self.resultado 
+  end
+
+end
