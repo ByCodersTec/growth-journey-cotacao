b8d033cc5d3bb2a166423afedc349a27e61fe8bc
rodrigocezzar <rodrigocezzar85@gmail.com>
2021-09-29 13:31:10 -0300

    api_mobile_conection

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -7,9 +7,10 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: 'json'} do 
     namespace :v1 do
       resources :quotations
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        sessions: 'api/v1/auth/sessions'
+      }
     end
   end
-
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
